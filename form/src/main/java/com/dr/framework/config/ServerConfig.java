84dbc246765156cbbba79c8599271cab079133b9
lich <lic@dr.com>
2020-04-13 15:50:57 +0800

    迁移功能

---
+++ form/src/main/java/com/dr/framework/config/ServerConfig.java
@@ -0,0 +1,34 @@
+package com.dr.framework.config;
+
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @Description: 获取项目IP和端口
+ * @Author: ZhangJR
+ * @CreateDate: 2019-11-12 17:47
+ */
+@Component
+public class ServerConfig implements ApplicationListener<WebServerInitializedEvent> {
+    private int serverPort;
+
+    public String getUrl() {
+        InetAddress address = null;
+        try {
+            address = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "http://" + address.getHostAddress() + ":" + this.serverPort;
+    }
+
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        this.serverPort = event.getWebServer().getPort();
+    }
+
+}

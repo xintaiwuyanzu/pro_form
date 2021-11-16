b9319a0ed31fb3488e4e6ed6ccfff6b83957bd45
haolsh <haolsh@dr.com>
2021-11-16 18:28:36 +0800

    删除前端无用代码，封装相关管理页面

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,22 +1,8 @@
-const path = require('path')
-const webpack = require('webpack')
 module.exports = {
-    pages: {
-        index: {entry: './src/main.js', title: '达融表单管理系统'},
-    },
-    productionSourceMap: false,
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@': path.join(process.cwd(), 'src')
-            }
-        },
-        plugins: [new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /zh-cn/)]
-    },
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8081/api',
+                target: 'http://127.0.0.1:8080/api',
                 pathRewrite: {'^/api': '/'}
             }
         }

a2495578375423cdfe6a5004741aa5e07ba8211b
caor <dr123456>
2023-02-20 11:44:00 +0800

    新增5个扩展字段ext1 - ext5

--- form/src/main/java/com/dr/framework/common/form/core/entity/FormDefinition.java
+++ form/src/main/java/com/dr/framework/common/form/core/entity/FormDefinition.java
@@ -55,7 +55,16 @@ public class FormDefinition extends BaseStatusEntity<String> implements FormMode
     @Column(name = "isBuildIn", comment = "是否内部使用")
     private boolean buildIn;
 
-
+    @Column(name = "ext1", comment = "扩展字段1")
+    private String ext1;
+    @Column(name = "ext2", comment = "扩展字段2")
+    private String ext2;
+    @Column(name = "ext3", comment = "扩展字段3")
+    private String ext3;
+    @Column(name = "ext4", comment = "扩展字段4")
+    private String ext4;
+    @Column(name = "ext5", comment = "扩展字段5")
+    private String ext5;
     private List<FormField> fields;
     private MetaMap meta;
 
@@ -128,10 +137,7 @@ public class FormDefinition extends BaseStatusEntity<String> implements FormMode
      */
     @JsonIgnore
     public Set<String> getFieldNames() {
-        return fields == null ? Collections.EMPTY_SET :
-                fields.stream()
-                        .map(FormField::getFieldCode)
-                        .collect(Collectors.toSet());
+        return fields == null ? Collections.EMPTY_SET : fields.stream().map(FormField::getFieldCode).collect(Collectors.toSet());
     }
 
     /**
@@ -141,10 +147,7 @@ public class FormDefinition extends BaseStatusEntity<String> implements FormMode
      */
     @JsonIgnore
     public Set<String> getFieldAlias() {
-        return fields == null ? Collections.EMPTY_SET :
-                fields.stream()
-                        .flatMap(formField -> formField.getFieldAlias().stream())
-                        .collect(Collectors.toSet());
+        return fields == null ? Collections.EMPTY_SET : fields.stream().flatMap(formField -> formField.getFieldAlias().stream()).collect(Collectors.toSet());
     }
 
 
@@ -270,5 +273,45 @@ public class FormDefinition extends BaseStatusEntity<String> implements FormMode
 
     public void setMeta(MetaMap meta) {
         this.meta = meta;
+    }
+
+    public String getExt1() {
+        return ext1;
+    }
+
+    public void setExt1(String ext1) {
+        this.ext1 = ext1;
+    }
+
+    public String getExt2() {
+        return ext2;
+    }
+
+    public void setExt2(String ext2) {
+        this.ext2 = ext2;
+    }
+
+    public String getExt3() {
+        return ext3;
+    }
+
+    public void setExt3(String ext3) {
+        this.ext3 = ext3;
+    }
+
+    public String getExt4() {
+        return ext4;
+    }
+
+    public void setExt4(String ext4) {
+        this.ext4 = ext4;
+    }
+
+    public String getExt5() {
+        return ext5;
+    }
+
+    public void setExt5(String ext5) {
+        this.ext5 = ext5;
     }
 }
